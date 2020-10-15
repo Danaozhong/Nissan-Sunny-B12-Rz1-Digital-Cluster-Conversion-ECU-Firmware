35a1669a9cdced550cca08556a1ed2bcb8d53baa
Clemens Zangl <clemens@texelography.com>
2020-10-15 21:27:00 +0200

    Minor Fixes
    - Moved the CAN stack to a different dirctory.
    - Inital commit of the version info code.

---
+++ Src/app/version_info/inc/version_info.hpp
@@ -0,0 +1,10 @@
+#ifndef _VERSION_INFO_HPP_
+#define _VERSION_INFO_HPP_
+
+namespace app
+{
+    const char* get_app_name();
+
+    const char* get_version_info();
+}
+#endif /* _VERSION_INFO_HPP_ */
---
+++ Src/app/version_info/src/version_info.cpp
@@ -0,0 +1,13 @@
+namespace app
+{
+    const char* get_app_name()
+    {
+        return "Nissan Sunny Rz1 Digital Cluster Converter";
+    }
+
+    const char* get_version_info()
+    {
+        return "v00.00.pre 2020_10_15";
+    }
+    
+}
